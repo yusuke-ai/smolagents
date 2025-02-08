e3df4a4f578102768323771b2203f1a4e5b059e3
yusuke-ai <yusuke.sawamura@hentoai.com>
2025-02-08 23:46:01 +0900

    test

--- src/smolagents/agents.py
+++ src/smolagents/agents.py
@@ -691,6 +691,8 @@ class ToolCallingAgent(MultiStepAgent):
             tool_arguments = tool_call.function.arguments
 
         except Exception as e:
+            import traceback
+            traceback.print_exc()
             raise AgentGenerationError(f"Error in generating tool call with model:\n{e}", self.logger) from e
 
         memory_step.tool_calls = [ToolCall(name=tool_name, arguments=tool_arguments, id=tool_call_id)]
